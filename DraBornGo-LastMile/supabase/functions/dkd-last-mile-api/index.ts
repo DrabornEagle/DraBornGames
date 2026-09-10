e3d64f8b98749d9f58a77e6c3adf1a704ceb8ef7
DrabornEagle <draborneagle@gmail.com>
2026-09-10 04:54:08 +0300

    release(lastmile): align backend API with v0.7

--- DraBornGo-LastMile/supabase/functions/dkd-last-mile-api/index.ts
+++ DraBornGo-LastMile/supabase/functions/dkd-last-mile-api/index.ts
@@ -1,6 +1,6 @@
 import { createClient } from 'npm:@supabase/supabase-js@2';
 
-const dkd_version = '0.6.1';
+const dkd_version = '0.7';
 const dkd_cors = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
